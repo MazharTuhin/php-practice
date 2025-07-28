d76e590b60102937bcbcb8c3f0ed517464a56dd6
Mazhar Tuhin <mazhartuhin78@gmail.com>
2025-07-28 22:16:15 +0600

    Step-13 (Session, error-success message, old value show) practice completed

--- step-12-session/index.php
+++ step-12-session/index.php
@@ -9,44 +9,101 @@
 <body>
 
 <?php
+    // Variable Declared
     $name = $email = $phone = '';
     $students = [];
+    $errors = [];
+    $success = '';
 
+
+    if(session_status() == PHP_SESSION_NONE) {
+        session_set_cookie_params(60);
+        session_start();
+    }
+
+    // If Server method POST and for submitted
     if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
-        $name = htmlspecialchars($_POST['name']);
-        $email = htmlspecialchars($_POST['email']);
-        $phone = htmlspecialchars($_POST['phone']);
-
-        if(session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
+        // Asign 'name', 'email', 'phone' fields values to pre declared variables
+        $name = trim(htmlspecialchars($_POST['name']));
+        $email = trim(htmlspecialchars($_POST['email']));
+        $phone = trim(htmlspecialchars($_POST['phone']));
 
         if (!isset($_SESSION['students'])) {
             $_SESSION['students'] = [];
         }
 
-        if (!empty($name) && !empty($email) && !empty($phone)) {
-            $newStudent = [
+        // ERROR MESSAGES
+        if (empty($name)) {
+            $errors['name'] = "Name is required";
+        }
+
+        if (empty($email)) {
+            $errors['email'] = "Email is required";
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = "Enter a valid email.";
+        }
+
+        if (empty($phone)) {
+            $errors['phone'] = "Phone is required!";
+        }
+
+        if (!empty($errors)) {
+            $_SESSION['errors'] = $errors;
+            $_SESSION['old_values'] = [
                 'name' => $name,
                 'email' => $email,
                 'phone' => $phone
             ];
-            
-            array_push($_SESSION['students'], $newStudent);
-        }
+        } else {
+            if (!isset($_SESSION['students'])) {
+                $_SESSION['students'] = [];
+            }
 
+            // make a array with the values of form fields and push these to sessions array
+            if (!empty($name) && !empty($email) && !empty($phone)) {
+                $newStudent = [
+                    'name' => $name,
+                    'email' => $email,
+                    'phone' => $phone
+                ];
+                
+                array_push($_SESSION['students'], $newStudent);
+                
+                // clear previous errors and old values
+                unset($_SESSION['errors']);
+                unset($_SESSION['old_values']);
+
+                // success Message
+                $_SESSION['success'] = 'New students information successfully added!';
+            }
+
+        }
+        
+        // for prevent duplicate on refresh page
         header("Location: " . $_SERVER['PHP_SELF']);
         exit();
     }
-
-    if(session_status() == PHP_SESSION_NONE) {
-        session_start();
+    // load data from session to $errors variable by GET request
+    if (isset($_SESSION['errors'])) {
+        $errors = $_SESSION['errors'];
+        unset($_SESSION['errors']);
+    }
+    // load data from session to $success variable by GET request
+    if (isset($_SESSION['success'])) {
+        $success = $_SESSION['success'];
+        unset($_SESSION['success']);
     }
 
+    // Old values
+    $old_name = isset($_SESSION['old_values']['name']) ? $_SESSION['old_values']['name'] : '';
+    $old_email = isset($_SESSION['old_values']['email']) ? $_SESSION['old_values']['email'] : '';
+    $old_phone = isset($_SESSION['old_values']['phone']) ? $_SESSION['old_values']['phone'] : '';
+
+
+    // load data from session to $students variable by GET request
     if (isset($_SESSION['students'])) {
         $students = $_SESSION['students'];
     }
-    // print_r($students);
 ?>
 
     <div class="container">
@@ -59,20 +116,46 @@
             <!-- MAIN FORM -->
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
                 <div class="form-group">
-                    <label for="name">Full Name</label>
-                    <input type="text" name="name" id="name" placeholder="Enter your name">
+                    <label for="name">Full Name<span>*</span></label>
+                    <input type="text" name="name" id="name" placeholder="Enter your name" value="<?php echo $old_name; ?>">
+
+                    <!-- Error Message -->
+                    <?php if (isset($errors['name'])): ?>
+                        <p class="error-message"> <?php echo $errors['name']; ?> </p>
+                    <?php endif; ?>
+
                 </div> <!-- NAME -->
                 <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="example@example.com">
+                    <label for="email">Email<span>*</span></label>
+                    <input type="email" name="email" id="email" placeholder="example@example.com" value="<?php echo $old_email; ?>">
+                        
+                    <!-- Error Message -->
+                    <?php if (isset($errors['email'])): ?>
+                        <p class="error-message"> <?php echo $errors['email']; ?> </p>
+                    <?php endif; ?>
+                
                 </div> <!-- EMAIL -->
                 <div class="form-group">
-                    <label for="phone">Phone</label>
-                    <input type="tel" name="phone" id="phone" placeholder="018-000-00000">
+                    <label for="phone">Phone<span>*</span></label>
+                    <input type="tel" name="phone" id="phone" placeholder="018-000-00000" value="<?php echo $old_phone; ?>">
+                        
+                    <!-- Error Message -->
+                    <?php if (isset($errors['phone'])): ?>
+                        <p class="error-message"> <?php echo $errors['phone']; ?> </p>
+                    <?php endif; ?>
+                
                 </div> <!-- PHONE -->
                 <div class="submit-btn">
                     <button type="submit" name="submit">Submit</button>
                 </div>
+
+                <!-- SUCCESS MESSAGE -->
+        
+                <?php if (isset($success)): ?>
+                    <div class="success-message">
+                        <?php echo $success; ?>
+                    </div>
+                <?php endif; ?>
             </form>
         </div>
 
